cdce4e312650a31bd492d9cbe8d91db21ee3e77a
eng wafaa <wafaajame@gmail.com>
2020-02-18 01:57:09 +0200

    comm

---
+++ app/src/main/java/com/shawerapp/android/screens/payment/GlobalData.java
@@ -0,0 +1,15 @@
+package com.shawerapp.android.screens.payment;
+
+import io.reactivex.Maybe;
+
+public class GlobalData {
+
+    public static String REGISTER_PHONE="REGISTER_PHONE";
+    public static  String ARG_QUESTION_DESCRIPTION = "ARG_QUESTION_DESCRIPTION";
+    public static Maybe<String> questionDescription;
+    public static Maybe<String> mComposition;
+    public static  CharSequence mCompositionchar;
+
+
+
+}
